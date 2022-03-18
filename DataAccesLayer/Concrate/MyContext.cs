46f53ea7754dd245afddd3f5ff96ac5775dda720
Aydoğan Selçuk Kılıçtaş <92909567+Sphinxs77@users.noreply.github.com>
2022-03-18 20:19:38 +0300

    Add project files.

---
+++ DataAccesLayer/Concrate/MyContext.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concrate;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccesLayer.Concrate
+{
+    public class MyContext:DbContext
+    {
+      protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      {
+           optionsBuilder.UseSqlServer("server=DESKTOP-9EHVTPP;database=MyProjectDb; integrated security=true;");
+            
+      }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Comment> Commnets { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+
+    }
+}
